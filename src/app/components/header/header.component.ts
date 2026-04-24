ade42427d0fa5fc0400271b64090b1e46a6efcaf
Bossa Web Solutions <gvfbossa@hotmail.com>
2026-04-24 16:53:20 -0300

    layout versao mobile

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,16 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
   scrollToZero() {
-    window.scrollTo(0,0)
+    window.scrollTo(0, 0)
+  }
+
+  menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
   }
 
 }
